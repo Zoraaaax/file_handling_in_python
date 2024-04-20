e79b3f184c3834982b9f85937ba11e90766392be
Kimberly <ae.xerx@gmail.com>
2024-04-20 20:32:39 +0800

    Separating the even numbers

--- separating_even_and_odd_numbers.py
+++ separating_even_and_odd_numbers.py
@@ -1,7 +1,8 @@
 #  Read
 with open("number.txt", "r") as file:
-    number = file.read()
+    numbers = file.read()
 #  Create
 numbers = [int(num.strip()) for num in numbers]
 #  Separate
+even_numbers = [num for num in numbers if num % 2 == 0]
 #  Write
