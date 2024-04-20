bad24e82ebc1b6f3627dddde51038d9b9c250efc
Kimberly <ae.xerx@gmail.com>
2024-04-20 22:08:29 +0800

    Writes the resulting string to the even file

--- separating_even_and_odd_numbers.py
+++ separating_even_and_odd_numbers.py
@@ -9,7 +9,7 @@ odd_numbers = [num for num in numbers if num % 2 != 0]
 #  Write
 with open("even.txt", "w") as even_file:
     for num in even_numbers:
-        even_file.write(str(num) + "\n")
+        even_file.write(str(num) + '\n')
 
 with open("odd.txt", "w") as odd_file:
     for num in odd_numbers:
