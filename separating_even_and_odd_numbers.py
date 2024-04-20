3bdd39db8f6b49e9bfa28aae2147d0516ed49cbf
Kimberly <ae.xerx@gmail.com>
2024-04-20 22:09:54 +0800

    Writes the resulting string to the odd file

--- separating_even_and_odd_numbers.py
+++ separating_even_and_odd_numbers.py
@@ -13,4 +13,4 @@ with open("even.txt", "w") as even_file:
 
 with open("odd.txt", "w") as odd_file:
     for num in odd_numbers:
-        odd_file.write(str(num) + "\n")
+        odd_file.write(str(num) + '\n')
