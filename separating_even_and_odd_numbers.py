4d0930f81df92053fb89d62e53af56e74690aa3d
Kimberly <ae.xerx@gmail.com>
2024-04-20 21:31:36 +0800

    Open the even.txt file

--- separating_even_and_odd_numbers.py
+++ separating_even_and_odd_numbers.py
@@ -7,5 +7,5 @@ numbers = [int(num.strip()) for num in numbers]
 even_numbers = [num for num in numbers if num % 2 == 0]
 odd_numbers = [num for num in numbers if num % 2 != 0]
 #  Write
-with open("even.txt", "w") as even_file:
+with open("even.txt", "r") as even_file:
     
