fe5898d3f912473524510ec9a273da25cb7813d5
Kimberly <ae.xerx@gmail.com>
2024-04-22 19:18:33 +0800

    Variable with empty string

--- students_highest_gwa.py
+++ students_highest_gwa.py
@@ -3,5 +3,6 @@ with open("students_name.txt", "r") as file:
     students_data = file.readlines()
 #  Initial Values
 highest_gwa = float('inf')
+students_name = ""
 #  Finding the Highest GWA
 #  Output
