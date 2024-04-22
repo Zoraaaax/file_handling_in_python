70c7f8906850505d61a0b71f326bb12b49a0ac56
Kimberly <ae.xerx@gmail.com>
2024-04-22 19:22:05 +0800

    Itteration for students_data

--- students_highest_gwa.py
+++ students_highest_gwa.py
@@ -3,6 +3,8 @@ with open("students_name.txt", "r") as file:
     students_data = file.readlines()
 #  Initial Values
 highest_gwa = float('inf')
-students_name = ""
+top_student_name = ""
 #  Finding the Highest GWA
+for data in students_data:
+    
 #  Output
