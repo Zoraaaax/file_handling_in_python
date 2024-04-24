a3da16bfb9b293bc6a5790ab7b4a70ef8023e41c
Kimberly <ae.xerx@gmail.com>
2024-04-25 00:03:06 +0800

    Opens the file double.txt in write mode

--- program_4_separating_even_double_and_odd_numbers_triple/separating_even_double_and_odd_numbers_triple.py
+++ program_4_separating_even_double_and_odd_numbers_triple/separating_even_double_and_odd_numbers_triple.py
@@ -7,4 +7,5 @@ numbers = [int(num.strip()) for num in numbers]
 square_even_numbers = [num ** 2 for num in numbers if num == 0]
 cube_odd_numbers = [num ** 3 for num in numbers if num != 0]
 #  Write
+with open("double.txt", "w") as double_even_file:
 #  Output
