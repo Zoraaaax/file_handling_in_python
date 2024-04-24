783588ef67035639299cc77f111473e24f237896
Kimberly <ae.xerx@gmail.com>
2024-04-25 00:25:35 +0800

    Triple and Separate the odd numbers

--- program_4_separating_even_double_and_odd_numbers_triple/separating_even_double_and_odd_numbers_triple.py
+++ program_4_separating_even_double_and_odd_numbers_triple/separating_even_double_and_odd_numbers_triple.py
@@ -6,7 +6,8 @@ numbers = [int(num.strip()) for num in numbers]
 
 #  Separate
 square_even_numbers = [num ** 2 for num in numbers if num % 2 == 0]
-cube_odd_numbers = [num ** 3 for num in numbers if num != 0]
+cube_odd_numbers = [num ** 3 for num in numbers if num % 2 != 0]
+
 #  Write
 with open("double.txt", "w") as double_even_file:
     for num in square_even_numbers:
