3e8c637b2ac30e9a2beb3fd027f259e2bd4717f7
Kimberly <ae.xerx@gmail.com>
2024-04-24 22:43:27 +0800

    Pseudocode

--- program_3_text_file_writer/text_file_writer.py
+++ program_3_text_file_writer/text_file_writer.py
@@ -1,7 +1,7 @@
 #  Write the file
 with open("mylife.txt", "w") as file:
 
-#  While loop
-        while True:
+    #  While loop
+    while True:
             line = input("Enter a line: ")
 #  Output
