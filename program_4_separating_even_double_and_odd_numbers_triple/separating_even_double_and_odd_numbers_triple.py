215aca8de8f58fe1a9da72fc6b4e225f11b33aa2
Kimberly <ae.xerx@gmail.com>
2024-04-25 00:04:48 +0800

    Itteration for square_even_numbers

--- program_4_separating_even_double_and_odd_numbers_triple/separating_even_double_and_odd_numbers_triple.py
+++ program_4_separating_even_double_and_odd_numbers_triple/separating_even_double_and_odd_numbers_triple.py
@@ -8,4 +8,5 @@ square_even_numbers = [num ** 2 for num in numbers if num == 0]
 cube_odd_numbers = [num ** 3 for num in numbers if num != 0]
 #  Write
 with open("double.txt", "w") as double_even_file:
+    for num in square_even_numbers:
 #  Output
