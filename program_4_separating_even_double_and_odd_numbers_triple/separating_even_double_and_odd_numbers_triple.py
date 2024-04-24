33f8ae72413bd5f0d00f17ef59c0f2855db05dd3
Kimberly <ae.xerx@gmail.com>
2024-04-24 23:47:07 +0800

    Double and Separate the even numbers

--- program_4_separating_even_double_and_odd_numbers_triple/separating_even_double_and_odd_numbers_triple.py
+++ program_4_separating_even_double_and_odd_numbers_triple/separating_even_double_and_odd_numbers_triple.py
@@ -4,6 +4,6 @@ with open("integers.txt" + "r") as file:
 #  Create
 numbers = [int(num.strip()) for num in numbers]
 #  Separate
-square_even_numbers = [num ** 2 for num in integer if num == 0]
+square_even_numbers = [num ** 2 for num in numbers if num == 0]
 #  Write
 #  Output
